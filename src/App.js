65a1fa04a7e75ddf2846b6c2135a2eaada5cf58e
Bruno <bruno@buddyweb.fr>
2024-10-12 22:33:16 +0200

    Add Manifest backend and frontend implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Manifest from "@mnfst/sdk";
+import { useEffect, useState } from "react";
 
 function App() {
+  // Import the Manifest SDK and create a new instance.
+  const manifest = new Manifest();
+
+  // Create a state to store the tweets.
+  const [tweets, setTweet] = useState([]);
+
+  useEffect(() => {
+    // Fetch the tweets and the user who created the tweet.
+    manifest
+      .from("tweets")
+      .with(["user"])
+      .find()
+      .then((tweets) => {
+        setTweet(tweets.data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ fontFamily: "Arial, sans-serif", padding: "10px" }}>
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        {tweets.map((tweet) => (
+          <li
+            key={tweet.createdAt}
+            style={{
+              border: "1px solid #e1e8ed",
+              padding: "10px",
+              borderRadius: "10px",
+              marginBottom: "10px",
+            }}
+          >
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <img
+                src={
+                  manifest.baseUrl.replace("api", "storage") +
+                  "/" +
+                  tweet.user.avatar.thumbnail
+                }
+                alt={`${tweet.user.name}'s avatar`}
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "50%",
+                  marginRight: "10px",
+                }}
+              />
+              <div>
+                <strong>{tweet.user.name}</strong>
+                <br />
+                <span style={{ color: "gray", fontSize: "12px" }}>
+                  {new Date(tweet.createdAt).toLocaleString()}
+                </span>
+              </div>
+            </div>
+            <p style={{ marginTop: "10px", marginBottom: "0" }}>
+              {tweet.content}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
